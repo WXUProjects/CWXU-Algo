569c75f7778b2499c9c4320f67a5a8ad15c26fa9
sanenchen <srcenchen@gmail.com>
2026-01-22 07:29:58 +0800

    chore(git): 修改gitignore

--- app/user/internal/data/redis/init.go
+++ app/user/internal/data/redis/init.go
@@ -4,12 +4,10 @@ import (
 	"cwxu-algo/app/common/conf"
 	"time"
 
-	"github.com/go-kratos/kratos/v2/log"
 	"github.com/redis/go-redis/v9"
 )
 
 func InitRedis(conf *conf.Data) *redis.Client {
-	log.Info(time.Duration(conf.Redis.ReadTimeout.Nanos))
 	rdb := redis.NewClient(&redis.Options{
 		Addr:         conf.Redis.Addr,
 		Password:     conf.Redis.Password,
