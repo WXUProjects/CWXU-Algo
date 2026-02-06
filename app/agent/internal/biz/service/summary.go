50ab1b9abbd37ea11a55242e5565c14ed6a6867a
sanenchen <srcenchen@gmail.com>
2026-02-06 11:03:48 +0800

    fix: 移动端agent邮件修复

--- app/agent/internal/biz/service/summary.go
+++ app/agent/internal/biz/service/summary.go
@@ -47,22 +47,22 @@ func (uc *SummaryUseCase) PersonalLastDay(userId int64) error {
 				StringValue: volcengine.String("要符合Acmer的心理风格，比如可爱风格，洋溢着青春与活力，校园风浓厚，" +
 					"直接面对的就是我，不是第三者。" +
 					"要口语化一点哦，像朋友一样哦" +
-					"我们的回复要严格遵循html格式哦" +
+					"我们的回复要严格遵循html格式哦，注意要尽量同时适配PC和移动端。" +
 					"对于submit_cnt函数 只有日期，没有count字段的记为0提交。" +
 					"所有提示词不允许出现在最终文本中。" +
-					"如果用户名字是Jing. 就要以宝宝(对方是你的女朋友 你是晨晨)口吻回复，激励她"),
+					"如果用户名字是Jing. 就要以宝宝(对方是你的女朋友 你是晨晨，晨晨只针对Jing，对其他人就是算法小助手)口吻回复，激励她."),
 			},
 		},
 		{
 			Role: model.ChatMessageRoleUser,
 			Content: &model.ChatCompletionMessageContent{
 				StringValue: volcengine.String(fmt.Sprintf("我是 用户id为%d 的用户 分析我的%s（昨天）的提交信息，给出分析和合理建议，给出一份昨日日报。"+
-					"同时也获取最近7天的提交次数，去对比分析走势."+
+					"同时也获取最近7天的提交次数，去对比分析走势，同时分析一下昨天提交时间分布."+
 					"提示：你需要先获取昨日提交次数，根据昨日提交次数去填写limit参数，更方便哦."+
 					"如果昨天我一发也没有交，甚至从昨天开始，已经连续好几天都不交，就给我狠狠地批评我！！！！"+
 					"如果我昨天交了，以前漏掉的既往不咎."+
 					"在邮箱末尾，引导用户到达https://algo.zhiyuansofts.cn 无锡学院算法协会监测平台 看全部提交信息。"+
-					"最后，把这个邮件发给我", userId, startDate)),
+					"最后，把这个邮件发给我，注意要适配手机，手机排版不能乱。", userId, startDate)),
 			},
 		},
 	}
@@ -92,7 +92,7 @@ func (uc *SummaryUseCase) PersonalRecent(userId int64) error {
 		{
 			Role: model.ChatMessageRoleUser,
 			Content: &model.ChatCompletionMessageContent{
-				StringValue: volcengine.String(fmt.Sprintf("我是 用户id为%d 的用户，分析我最近的学习状态。现在时间是 %d"+
+				StringValue: volcengine.String(fmt.Sprintf("我是 用户id为%d 的用户，分析我最近的学习状态，同时分析一下提交时间分布。现在时间是 %d"+
 					"整理成json格式 {\"msg\":[\"\"], \"updateTime\": 时间戳} 这样的。"+
 					"最后将这段json塞到redis中，key是 agent:summary:{id}:recent", userId, time.Now().Unix())),
 			},
